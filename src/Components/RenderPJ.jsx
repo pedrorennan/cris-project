d1c2d375e11dde51091cd781cd1ab99eab33c728
Pedro Rennan <pedrorennan4444@gmail.com>
2026-02-27 14:18:56 -0300

    style: melhoria simples no estilo da pagina de agentes

--- src/Components/RenderPJ.jsx
+++ src/Components/RenderPJ.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react"
-import regras from "../Data/rules.json"
 
 function RenderPJ() {
     const [agents, setAgents] = useState([])
--- src/Pages/Agents/index.jsx
+++ src/Pages/Agents/index.jsx
@@ -1,23 +1,28 @@
 import CreatePJ from "../../Components/CreatePJ";
-import agentsList from "./agents.json"
 import RenderPJ from "../../Components/RenderPJ";
 import './style.css'
 import { useState, useEffect } from "react";
 
-function Agents({toHome}) {
+function Agents({ toHome }) {
     const [showSheet, setShowSheet] = useState(true)
 
     return (
         <div className="sheet-container">
-            <h1>C.R.I.S</h1>
-            <h2>Seus Agentes</h2>
-            {showSheet ? (
-                <RenderPJ />
-            ) : (
-                <CreatePJ sheetDone={() => setShowSheet(true)}/>
-            )}
-            <button onClick={() => setShowSheet(false)}>Criar Novo Agente</button>
-            <button onClick={toHome}>Voltar para a página inicial</button>
+            <header>
+                <h1>C.R.I.S</h1>
+                <h2>Seus Agentes</h2>
+            </header>
+            <main>
+                {showSheet ? (
+                    <RenderPJ />
+                ) : (
+                    <CreatePJ sheetDone={() => setShowSheet(true)} />
+                )}
+                <button onClick={() => setShowSheet(false)}>Criar Novo Agente</button>
+            </main>
+            <footer>
+                <button onClick={toHome}>Voltar para a página inicial</button>
+            </footer>
         </div>
     )
 }
