c4e6b61de5a715f1478aa51b3b262f89010c4909
Pedro Rennan <pedrorennan4444@gmail.com>
2026-02-28 14:47:28 -0300

    feat: Sistema de rolar atributos com valor 0

--- src/Utils/RollDice.js
+++ src/Utils/RollDice.js
@@ -1,15 +1,23 @@
 const RollDice = (dice, nDices) => {
-    let resultado = 0;
+    let resultado = [];
 
-    for (let i = 1; i <= nDices; i++) {
-        let diceResult = Math.floor(Math.random() * dice) + 1;
-        console.log(`Resultado ${i}: ${diceResult}`)
-        if (diceResult > resultado) {
-            resultado = diceResult;
+    if (nDices <= 0) {
+        let diceResult1 = Math.floor(Math.random() * dice) + 1;
+        let diceResult2 = Math.floor(Math.random() * dice) + 1;
+        resultado.push(diceResult1, diceResult2);
+    } else {
+        for (let i = 1; i <= nDices; i++) {
+            let diceResult = Math.floor(Math.random() * dice) + 1;
+            resultado.push(diceResult);
+            console.log(`Resultado ${i}: ${diceResult}`)
+            if (diceResult > resultado) {
+                resultado[i-1] = diceResult;
+            }
         }
     }
 
-    alert(resultado)
+
+    alert(`Resultados: ${resultado}`)
 }
 
 export default RollDice
